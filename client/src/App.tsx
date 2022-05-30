968bded910d6acc669d54e5d370eaa9bed9a00b3
2018009016 <2018009016@hanyang.ac.kr>
2022-05-31 01:30:00 +0900

    feat: top50 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
-import Top50Page from './pages/Top50Page';
 import './styles/common.scss';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Top50Page />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
